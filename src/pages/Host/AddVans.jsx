27f469763661ef386d3747c2f3293a78838312fd
patboy04 <theofficialjp@gmail.com>
2023-12-06 17:34:28 +0800

    Optimized addVans (without css yet)

--- src/pages/Host/AddVans.jsx
+++ src/pages/Host/AddVans.jsx
@@ -1,6 +1,7 @@
 import React from "react"
-import { Form, useNavigation } from "react-router-dom"
+import { Form, useNavigation, redirect } from "react-router-dom"
 import { addVans } from "../../api"
+import "../../utils"
 
 
 export async function action({ request }) {
@@ -12,13 +13,14 @@ export async function action({ request }) {
         type: formData.get("type")
     }
 
-    console.log(vanObject)
     try {
         addVans(vanObject)
-    } catch (error) {
-        console.log(error)
+        const res = redirect("vans")
+        res.body = true;
+        return res
+    } catch (err) {
+        return err.message
     }
-    return null
 }
 
 export default function AddVans() {
@@ -26,31 +28,36 @@ export default function AddVans() {
     const status = navigation.state
 
     return (
-        <Form method="post" className="addVan--form--container" replace={true}>
-            <input
-                name="name"
-                type="text"
-                placeholder="Name of Van"
-            />
-            <input
-                name="description"
-                type="text"
-                placeholder="Description"
-            />
-            <input
-                name="price"
-                type="text"
-                placeholder="Price"
-            />
-            <input
-                name="type"
-                type="text"
-                placeholder="type"
-            />
-            <button disabled={status === "submitting"} className="home--main--button">
-                {status === "submitting" ? "Processing..." : "Add Vans"}
-            </button>
-        </Form>
+        <div>
+            <Form method="post" className="addVan--form--container" replace={true}>
+                <h1>Add You Van!</h1>
+                <input
+                    name="name"
+                    type="text"
+                    placeholder="Name of Van"
+                />
+                <input
+                    name="description"
+                    type="text"
+                    placeholder="Description"
+                />
+                <input    
+                    name="price" 
+                    type="number" 
+                    placeholder="$10"
+                />
+                <select name="type">
+                    <option value="rugged">Rugged</option>
+                    <option value="luxury">Luxury</option>
+                    <option value="simple">Simple</option>
+                </select>
+                <button disabled={status === "submitting"} className="home--main--button">
+                    {status === "submitting" ? "Processing..." : "Add Vans"}
+                </button>
+             </Form>
+
+        </div>
+        
     )
 }
 
